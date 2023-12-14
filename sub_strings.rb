1f469ed1c6bb5ae1cf568c2e7f3c440f595d4acc
“Baurzhaan” <102077363+baurzhaan@users.noreply.github.com>
2023-12-14 16:21:21 +0000

    print a word and an array of valid substrings

--- sub_strings.rb
+++ sub_strings.rb
@@ -1,4 +1,4 @@
-=begin
+=begin https://www.theodinproject.com/lessons/ruby-sub-strings
 
 Implement a method #substrings that takes a word as the first argument and then an array of valid substrings (your dictionary) as the second argument. It should return a hash listing each substring (case insensitive) that was found in the original string and how many times it was found.
 
@@ -16,3 +16,12 @@ Next, make sure your method can handle multiple words:
 
 =end
 
+def substring(word, substrings)
+  # print a word and array of valid substrings
+  puts word
+  puts substrings
+end
+
+word = "below"
+dictionary = %w[below,down,go,going,horn,how,howdy,it,i,low,own,part,partner,sit]
+substring(word, dictionary)
